319a3cbca4efad1eaf8dffd0c3026d3a04eaa4ef
wazave <jelizald@compusoluciones.com>
2016-11-23 16:22:53 -0600

    C
    C

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,15 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
 
 gulp.task('serve', ['sass'], () => {
-
     browserSync.init({
         server: './'
     });
-
     gulp.watch(['./components/*.scss', './base/*.scss', './*.scss'], ['sass']);
     gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src(['./components/*.scss', './base/*.scss', './*.scss'])
         .pipe(sass())
---
+++ style.js
@@ -0,0 +1,12 @@
+// $(document).ready(() => {
+//   adjustHeader();
+//   console.log('hey');
+// });
+
+// $(window).resize(adjustHeader);
+
+// function adjustHeader() {
+//   console.log('ki')
+//   let height = $('#headr').height();
+//   $('#content').css('padding-top', height);
+// };
