88ec894e4fe76d283ed6c7f4e928f67462edea7c
wazave <jelizald@compusoluciones.com>
2016-11-24 16:42:22 -0600

    F
    g

--- style.js
+++ style.js
@@ -1,7 +1,28 @@
-// $(document).ready(() => {
-//   adjustHeader();
-//   console.log('hey');
-// });
+/* eslint-disable no-undef */
+$(document).ready(() => {
+  $('.img-select2').css('display', 'none');
+  headerFunc();
+});
+
+function headerFunc () {
+  $('.img-select, .img-select2').hover(() => {
+    $('.img-select').css('display', 'none');
+    $('.img-select2').css('display', 'block');
+    $('.tbig').css('color', '#C8C7C6'); //#9A999E $('body').css('color')
+  }, () => {
+    $('.img-select').css('display', 'block');
+    $('.img-select2').css('display', 'none');
+    $('.tbig').css('color', 'inherit');
+  });
+  // $('.tbig').hover((e) => {
+  //   $('.tbig').css('color', '#C8C7C6');
+  //   $(e.target).css('color', 'inherit');
+  // }, () => {
+  //   $('.tbig').css('color', 'inherit');
+  // });
+}
+
+
 
 // $(window).resize(adjustHeader);
 
