c3fbdb520e800de43980c8f8a6fdd2c0afdb1461
wazave <jelizald@compusoluciones.com>
2016-12-02 16:36:23 -0600

    L
    L

--- style.js
+++ style.js
@@ -2,32 +2,39 @@
 $(document).ready(() => {
   $('.img-select2').css('display', 'none');
   headerFunc();
+  adjustLogo();
 });
 
 function headerFunc () {
   $('.img-select, .img-select2').hover(() => {
     $('.img-select').css('display', 'none');
     $('.img-select2').css('display', 'block');
-    $('.tbig').css('color', '#C8C7C6'); //#9A999E $('body').css('color')
+    $('.text-big').css('color', '#C8C7C6'); // #9A999E $('body').css('color')
   }, () => {
     $('.img-select').css('display', 'block');
     $('.img-select2').css('display', 'none');
-    $('.tbig').css('color', 'inherit');
+    $('.text-big').css('color', '');
   });
-  // $('.tbig').hover((e) => {
-  //   $('.tbig').css('color', '#C8C7C6');
-  //   $(e.target).css('color', 'inherit');
-  // }, () => {
-  //   $('.tbig').css('color', 'inherit');
-  // });
+  $(window).resize(() => {
+    adjustLogo();
+  });
+  console.log($('header').height());
+  $('body').css('padding-top', $('header').height() + 24);
+
+  $('#hamburger-menu').click(() => {
+    console.log($('a .hide'));
+    if ($('a .hide .show').length > 0) {
+      $('a .hide .show').removeClass('show');
+    } else {
+      $('a .hide').addClass('show');
+    }
+  });
 }
 
-
-
-// $(window).resize(adjustHeader);
-
-// function adjustHeader() {
-//   console.log('ki')
-//   let height = $('#headr').height();
-//   $('#content').css('padding-top', height);
-// };
+function adjustLogo () {
+  if ($(window).width() < 768) {
+    $('#header-logo').addClass('horizontal-left');
+  } else {
+    $('#header-logo').removeClass('horizontal-left');
+  }
+}
